a8f865f730efc1a3b5e06624d848809008c24def
leoberteck <leoberteck@gmail.com>
2016-11-18 20:36:05 -0200

    Criada função que remove acentos de strings. Falta chamar essa função nos daos. nas funções update e insert.

--- src/main/java/br/edu/ifsp/bri/euexisto/util/StringUtil.java
+++ src/main/java/br/edu/ifsp/bri/euexisto/util/StringUtil.java
@@ -5,7 +5,11 @@
  */
 package br.edu.ifsp.bri.euexisto.util;
 
+import java.lang.reflect.Field;
 import java.text.Normalizer;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -25,4 +29,23 @@ public class StringUtil {
         }
         return result;
     }
+    
+    public static <T> T RemoveAcentoFromT(T instance){
+        Field[] listField = instance.getClass().getDeclaredFields();
+        for(Field _field : listField){
+            if(_field.getGenericType() == String.class)
+            {
+                _field.setAccessible(true);
+                try {
+                    String value = _field.get(instance).toString();
+                    value = RemoveAcento(value);
+                    _field.set(instance, value);
+                } catch (IllegalArgumentException | IllegalAccessException ex) {
+                    Logger.getLogger(StringUtil.class.getName()).log(Level.SEVERE, "On field : " + _field.getName(), ex);
+                }
+                _field.setAccessible(false);
+            }
+        }
+        return instance;
+    }   
 }
--- src/test/java/br/edu/ifsp/bri/euexisto/StringUtilTest.java
+++ src/test/java/br/edu/ifsp/bri/euexisto/StringUtilTest.java
@@ -5,6 +5,7 @@
  */
 package br.edu.ifsp.bri.euexisto;
 
+import br.edu.ifsp.bri.euexisto.domain.Cidadao;
 import br.edu.ifsp.bri.euexisto.util.StringUtil;
 import java.text.ParseException;
 
@@ -13,25 +14,45 @@ import java.text.ParseException;
  * @author leo
  */
 public class StringUtilTest {
-    public static void main (String args[]) throws ParseException   {    
-        System.out.println("Validação de remoção de acentos : " + TesteRemoveAcento());  
-    }
     
-    private static String TesteRemoveAcento(){
-        String result = "OK";
-        String[] acentos = new String[]{
+    static final String[] ACENTOS = new String[]{
         "Á", "á", "Â", "â", "À","à", "Å", "å", "Ã", "ã", "Ä", 
         "ä", "Æ", "æ", "ï", "É", "é", "Ê", "ê", "Ë", "ë", "Í", 
         "î", "Ì", "ì", "Ï", "Ù", "ó", "Ô", "È", "è", "ò", "Ø", 
         "ð", "Ó", "í", "Î", "ô", "Ò", "õ", "Ö", "Ú", "û", "ù", 
         "Ü", "ø", "Ç", "ç", "Ñ", "ñ", "Õ", "Ý", "ý", "ö", "\"",
         "<", "ú", "Û", "®", "©", "Þ", "ü", ">", "&", "þ", "ß"};
+    
+    public static void main (String args[]) throws ParseException   {    
+        System.out.println("Validação de remoção de acentos : " + TesteRemoveAcento());  
+        System.out.println("Validação de remoção de acentos de um obejto : " + TesteRemoveAcentoFromT());
+    }
+    
+    private static String TesteRemoveAcento(){
+        String result = "OK";
+        
         String resultStr = StringUtil.RemoveAcento("ÁáÂâÀàÅåÃãÄäÆæïÉéÊêËëÍîÌìÏÙóÔÈèòØðÓíÎôÒõÖÚûùÜøÇçÑñÕÝýö<úÛ®©Þü>&þß");
-        if(StringUtil.Contains(resultStr, acentos, true))
+        if(StringUtil.Contains(resultStr, ACENTOS, true))
         {
             result = "Falhou no teste de remoção de caracteres acentuados";
         }
         System.out.println(resultStr);
+        return result;
+    }
+    
+    private static String TesteRemoveAcentoFromT(){
+        String result = "OK";
+        
+        Cidadao mock = new Cidadao();
+        mock.setNome("ÁáÂâÀàÅåÃãÄäÆæïÉéÊêËëÍîÌìÏÙóÔÈèòØðÓíÎôÒõÖÚûùÜøÇçÑñÕÝýö<úÛ®©Þü>&þß");
+        mock.setEmail("ÁáÂâÀàÅåÃãÄäÆæïÉéÊêËëÍîÌìÏÙóÔÈèòØðÓíÎôÒõÖÚûùÜøÇçÑñÕÝýö<úÛ®©Þü>&þß");
+        
+        mock = StringUtil.RemoveAcentoFromT(mock);
+        if(StringUtil.Contains(mock.getNome(), ACENTOS, true) || 
+        StringUtil.Contains(mock.getEmail(), ACENTOS, true)){
+            result = "Falhou no teste de remoção de caracteres acentuados de um objeto";
+        }
+        
         return result;
     }
 }
